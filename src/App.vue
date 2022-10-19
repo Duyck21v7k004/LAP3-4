d0fb032cd4f9c7e3ce5ab529e2f72c8ec7da8eae
DuyNguyen <Duyck21v7k004@vlvh.ctu.edu.vn>
2022-10-19 08:17:20 +0700

    Khoi tao du an

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<script>
+export default {
+}
+</script>
+<template>
+<h1>Hello, Vue.js!</h1>
+</template>
+<style>
+.page {
+max-width: 400px;
+margin: auto;
+}
+</style>
